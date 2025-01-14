bf46f75157c444d8cdfdf8549cee3010e2bfdfa5
aochan <nakinagaraodoru1013@gmail.com>
2025-01-14 21:46:49 +0900

    Goal生成時に日付の形式のチェック処理を追加

--- app/core/goal.go
+++ app/core/goal.go
@@ -1,10 +1,20 @@
 package core
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 func NewGoal(title string, endDate string) (Goal, error) {
 	if title == "" {
 		return Goal{}, errors.New("title is required")
+	}
+
+	YMDGolangDateFormat := "2006-01-02"
+	_, err := time.Parse(YMDGolangDateFormat, endDate)
+
+	if err != nil {
+		return Goal{}, errors.New("endDate is invalid")
 	}
 
 	return Goal{
--- app/core/goal_test.go
+++ app/core/goal_test.go
@@ -28,3 +28,47 @@ func Test_目標の日付を確認できる(t *testing.T) {
 
 	assert.Equal(t, true, goal.SameEndDate("2025-01-01"))
 }
+
+func Test_目標の日付の形式が正しくない場合にエラー(t *testing.T) {
+	cases := []struct {
+		name              string
+		invalidDateFormat string
+	}{
+		{
+			name:              "文字列",
+			invalidDateFormat: "文字列形式",
+		},
+		{
+			name:              "年のみ",
+			invalidDateFormat: "2025",
+		},
+		{
+			name:              "年月",
+			invalidDateFormat: "2025-01",
+		},
+		{
+			name:              "年月日時",
+			invalidDateFormat: "2025-01-01 00",
+		},
+		{
+			name:              "年月日時分",
+			invalidDateFormat: "2025-01-01 00:00",
+		},
+		{
+			name:              "年月日時分秒",
+			invalidDateFormat: "2025-01-01 00:00:00",
+		},
+		{
+			name:              "形式が変",
+			invalidDateFormat: "2025/01/01",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewGoal("Title", tt.invalidDateFormat)
+
+			assert.Error(t, err)
+		})
+	}
+}
