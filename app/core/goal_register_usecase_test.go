0547ad883d2e01ed41a9f00c915f6e7692e5c38a
aochan <nakinagaraodoru1013@gmail.com>
2025-01-24 01:02:51 +0900

    モックリポジトリが正しく動くかのテストと実装を追加

--- app/core/goal_register_usecase_test.go
+++ app/core/goal_register_usecase_test.go
@@ -7,11 +7,41 @@ import (
 )
 
 func Test_目標を登録する(t *testing.T) {
+	t.Skip("モックリポジトリのアサート関数が正しく動くテストが動いてから実装")
 	db := newMockGoalRepository()
 	usecase := core.NewGoalRegisterUsecase(&db)
 	usecase.Execute("Title", "2025-01-01")
 
 	assert.True(t, db.calledRegister(1, "Title", "2025-01-01"), true)
+}
+
+func Test_モックリポジトリのアサートが正しく動くかのテスト(t *testing.T) {
+	mock := newMockGoalRepository()
+	mock.Register("Title", "2025-01-01")
+
+	assert.True(t, mock.calledRegister(1, "Title", "2025-01-01"))
+}
+
+func Test_モックリポジトリのアサートが失敗した時のテスト(t *testing.T) {
+	mock := newMockGoalRepository()
+	mock.Register("Title", "2025-01-01")
+
+	assert.False(t, mock.calledRegister(2, "Title", "2025-01-01"))
+}
+
+func Test_モックリポジトリのアサートが回数があっているが引数が異なっている場合に失敗するテスト(t *testing.T) {
+	mock := newMockGoalRepository()
+	mock.Register("Title", "2025-01-01")
+
+	assert.False(t, mock.calledRegister(1, "not match title", "2025-01-01"))
+}
+
+func Test_モックリポジトリの2回以上コールしたときに最後の引数が異なっている場合に失敗するテスト(t *testing.T) {
+	mock := newMockGoalRepository()
+	mock.Register("Title", "2025-01-01")
+	mock.Register("Title2", "2025-01-02")
+
+	assert.False(t, mock.calledRegister(2, "Title2", "2025-01-03"))
 }
 
 func newMockGoalRepository() *mockGoalRepository {
@@ -26,14 +56,27 @@ type mockGoalRepository struct {
 }
 
 func (a *mockGoalRepository) calledRegister(count int, title string, endDate string) bool {
-	return false
+	if len(a.calledRegisterArgs) != count {
+		return false
+	}
+
+	for i := 0; i < count; i++ {
+		currentArg := a.calledRegisterArgs[i]
+
+		if currentArg.title != title || currentArg.endDate != endDate {
+			return false
+		}
+	}
+
+	return true
 }
 
-func (a *mockGoalRepository) get(index int) inMemoryGoal {
-	return inMemoryGoal{}
-}
-
-type inMemoryGoal struct {
-	title   string
-	endDate string
+func (a *mockGoalRepository) Register(title, endDate string) {
+	a.calledRegisterArgs = append(a.calledRegisterArgs, struct {
+		title   string
+		endDate string
+	}{
+		title:   title,
+		endDate: endDate,
+	})
 }
